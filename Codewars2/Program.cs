dc0ebca7c28bd514a6e228c3438460a1b07cbe78
Zach Brown <bigcountry66789@gmail.com>
2026-03-24 15:47:35 -0500

    Create a new Codewars exercise repo for my practice projects

---
+++ Codewars2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.RegularExpressions;
+using System.Linq; 
+namespace Codewars2;
+
+class Program
+{
+    //Duplicate Count: Return the number of duplicates that occur inside a string
+    public static int DuplicateCount(string str)
+    {
+        return str.Contains("a") ? str.Count(c => c == 'a') : str.Count(c => c == 'b');
+    }
+    
+    //Break the camelCase
+    public static string BreakCamelCase(string str) =>
+    
+        new Regex("([A-Z])").Replace(str, " $1");
+         /*Instead of looking for a specific transition(like lowercase to uppercase) it treats every single
+          uppercase letter as a trigger to insert a space
+          My original code: Regex.Replace(str, "([a-z])([A-Z])", "$1 $2") looked for a transition from lower to upper*/
+         
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        
+        //BreakCamelCase Method Test
+        Console.WriteLine($"'camelCase' -> '{BreakCamelCase("camelCase")}'");
+        Console.WriteLine($"'NASA' -> '{BreakCamelCase("NASA")}'");
+        Console.WriteLine($"'SomeABCWord' -> '{BreakCamelCase("SomeABCWord")}'");
+        
+        //DuplicateCount Method Test 
+        Console.WriteLine($"$aabbccdd {DuplicateCount("aabbccdd")}");
+        Console.WriteLine($"$abcde {DuplicateCount("abcde")}");
+        Console.WriteLine($"$aAbBcdef {DuplicateCount("aAbBcdef")}");
+        Console.WriteLine($"Mississippi {DuplicateCount("Mississippi")}");
+    }
+}
